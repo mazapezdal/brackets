79ae267f40466f275228ec1ca21887e3057e4e35
Vladislav Mazannik <mazanniknnik@gmail.com>
2022-08-12 17:39:17 +0300

    feat: my brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let arrayJoinBrackets = [];
+  for (i = 0; i < bracketsConfig.length; i++) {
+    arrayJoinBrackets.push(bracketsConfig[i].join(''));
+  }
+  console.log(arrayJoinBrackets);
+
+  let newStr = str;
+  for (i = 0; i < arrayJoinBrackets.length; i++) {
+    if (newStr.includes(arrayJoinBrackets[i])) {
+      newStr = newStr.replace(arrayJoinBrackets[i], '')
+
+      i = -1
+    }
+  }
+
+  if(newStr == ''){
+    return true;
+  } else{
+    return false;
+  }
+ 
 }
